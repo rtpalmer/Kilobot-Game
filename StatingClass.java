bca2ae79d56b26ef3a96257bab4c2e3e476b7f82
Thomas Palmer <rtpalmer4@gmail.com>
2013-08-04 20:54:59 -0500

    Create StatingClass.java
    Here i imported the Applet method for the use of the superclass, and extended the starting class so that i could us Applets methods.

---
+++ StatingClass.java
@@ -0,0 +1,37 @@
+package kiloboltgame;
+
+// Imported Applet for the use of the superclass method.
+import java.applet.Applet;
+import java.awt.Graphics;
+
+// Extended the starting class with applet so that it could use applets methods.
+public class StartingClass extends Applet {
+
+  
+	@Override //Overrides method from a parent class.
+	public void init() {
+		
+		super.init();
+	}
+	
+	@Override
+	public void start() {
+		
+		super.start()
+		
+	}
+	
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+	
+	
+}
